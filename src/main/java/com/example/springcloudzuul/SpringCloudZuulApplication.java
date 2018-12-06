9e49d7a184df7a009f5b1fd4c36969c8e4c7daef
Tianqi Ren <tqren@thoughtworks.com>
2018-12-06 14:09:03 +0800

    [colin] zuul add filter

--- src/main/java/com/example/springcloudzuul/SpringCloudZuulApplication.java
+++ src/main/java/com/example/springcloudzuul/SpringCloudZuulApplication.java
@@ -1,14 +1,20 @@
 package com.example.springcloudzuul;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
-@SpringBootApplication
+@SpringCloudApplication
 public class SpringCloudZuulApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudZuulApplication.class, args);
+        new SpringApplicationBuilder(SpringCloudZuulApplication.class).web(true).run(args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
     }
 }
